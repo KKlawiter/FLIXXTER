c3f657578ef851bcaeddfa1d30d9abcbb1a61944
KKlawiter <katrinaklawiter@gmail.com>
2020-01-11 23:54:54 +0000

    add image and video uploading

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,10 +1,10 @@
 class Course < ApplicationRecord
-	mount_uploader :image, ImageUploader
-	belongs_to :user
-	has_many :sections
-	has_many :enrollments
+  belongs_to :user
+  mount_uploader :image, ImageUploader
+  has_many :sections
+  has_many :enrollments
 
-	validates :title, presence: true
-  	validates :description, presence: true
-  	validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,11 @@ class User < ApplicationRecord
   has_many :enrollments
   has_many :enrolled_courses, through: :enrollments, source: :course
 
+  
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :validatable
 
   def enrolled_in?(course)
     return enrolled_courses.include?(course)
